97219269d1294883c242cfabec81d1b8485bf212
Theresa Manney <theresa.m.17@gmail.com>
2017-07-06 14:52:34 -0700

    simple cometition model tests WIP, 2 passing tests

--- test/models/competition_test.rb
+++ test/models/competition_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Competition do
-  let(:competition) { Competition.new }
+  let(:new_competition) { Competition.new }
 
-  it "must be valid" do
-    value(competition).must_be :valid?
+  describe "Competition relationships" do
+    it "competitions belong to a trip" do
+      skip
+    end
+  end
+
+  describe "Competition valdations" do
+    it "is valid with trip_id, level and comp_type" do
+      competition = competitions(:comp_two)
+      competition.valid?.must_equal true
+    end
+
+    REQUIRED_FIELDS = %w(trip_id level comp_type)
+
+    REQUIRED_FIELDS.each do |field|
+      it "Competition is not valid when #{field} id not present" do
+        new_competition.valid?.must_equal false
+        new_competition.errors.messages.must_include field.to_sym
+      end
+    end
   end
 end
