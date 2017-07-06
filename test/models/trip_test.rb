604b278e92b5e7ddcdde1b4d5e7014486bbc040c
Theresa Manney <theresa.m.17@gmail.com>
2017-07-06 15:38:33 -0700

    trip model relationship tests created and are passing

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,9 +1,40 @@
 require "test_helper"
 
 describe Trip do
-  let(:trip) { Trip.new }
+  let(:new_trip) { Trip.new }
+  let(:a_trip) { Trip.new(
+    traveler_id: travelers(:traveler_one).id,
+    location: "Denver",
+    event_name: "Camp Hollywood",
+    start_date: trips(:trip_one).start_date,
+    end_date: trips(:trip_one).end_date,
+    event_paid: true,
+    hotel_reserved: true,
+    flight_paid: true
+    )
+  }
 
-  it "must be valid" do
-    value(trip).must_be :valid?
+  REQUIRED_FIELDS = %w(traveler_id location start_date end_date)
+
+  describe "Trip relationships" do
+    it "Trip can have many competitions" do
+      the_trip = trips(:trip_one)
+      the_trip.competitions.must_include competitions(:comp_one)
+      the_trip.competitions.must_include competitions(:comp_three)
+    end
+
+    it "Trip has the correct traveler" do
+      a_trip.must_respond_to :traveler
+      a_trip.traveler.must_be_kind_of Traveler
+    end
   end
+
+  describe "Trip validations" do
+  end
+
 end
+
+# validates :traveler_id, presence: true
+# validates :location, presence: true
+# validates :start_date, presence: true
+# validates :end_date, presence: true
