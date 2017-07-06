3f29eedcbcd4b8c7f82b1b78a6be291d2f4545c7
Theresa Manney <theresa.m.17@gmail.com>
2017-07-05 17:00:43 -0700

    completed some controller tests for the index of the competition controller

--- test/controllers/competitions_controller_test.rb
+++ test/controllers/competitions_controller_test.rb
@@ -2,4 +2,37 @@ require "test_helper"
 
 describe CompetitionsController do
   KEYS = %w(id level comp_type finals placement trip_id)
+
+  describe "index" do
+    it "is a real working route" do
+      get competitions_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get competitions_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get competitions_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    # TODO: will need to change this to be more specific to returning all competitions with similar traveler and trip ids combined
+    it "returns all of the competitions" do
+      get competitions_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Competition.count
+    end
+
+    it "returns competitions with exactly the required fields" do
+      get competitions_url
+      body = JSON.parse(response.body)
+      body.each do |competition|
+        competition.keys.sort.must_equal KEYS.sort
+      end
+    end
+  end
 end
